e49c8bbda948ed2b7baab06938b0850e095ef3c7
Antonio-Meza <153248571+Antonio-Meza@users.noreply.github.com>
2024-10-20 23:58:01 -0500

    detalle de contactanos

--- lib/src/pages/ContactosScreen.dart
+++ lib/src/pages/ContactosScreen.dart
@@ -5,18 +5,31 @@ class ContactosScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Contactos'),
+        title: Text('Información de Contacto'),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Pantalla de Contactos'),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('Volver a Inicio'),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Ciudad: Quito',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Teléfonos: 3814330 / 0983575019',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Email: info@repuestos247.com',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Web: www.repuestos247.com',
+              style: TextStyle(fontSize: 18),
             ),
           ],
         ),
